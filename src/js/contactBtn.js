3cad77181beb68af6a27b78f8eee19fc599ef523
Sergiy Tyschenko <sergey.tyschenko@gmail.com>
2025-10-19 22:12:49 +0300

    contactbtn added

---
+++ src/js/contactBtn.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const hero = document.querySelector('.header');
+  const feedbackBtn = document.getElementById('feedback-btn');
+console.log(feedbackBtn);
+  const observer = new IntersectionObserver(
+    entries => {
+      entries.forEach(entry => {
+        console.log(feedbackBtn);
+        // Якщо hero-секція видима — ховаємо кнопку
+        if (entry.isIntersecting) {
+          feedbackBtn.classList.remove('visible');
+        } else {
+          // Якщо користувач прокрутив нижче hero — показуємо кнопку
+          feedbackBtn.classList.add('visible');
+        }
+      });
+    },
+    { threshold: 0.1 }
+  );
+
+  observer.observe(hero);
+});
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import './js/email';
 import './js/validation';
 import './js/menu';
+import './js/contactBtn';
